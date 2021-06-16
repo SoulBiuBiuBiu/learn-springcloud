901ae2ca31a2a9f5ba2d8f4591456bb0f425c0ed
Barry <1172807506@qq.com>
2021-06-16 11:15:33 +0800

    Hystrix DashBoard,解决404问题

--- service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
@@ -1,7 +1,9 @@
 package com.example.serviceribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -24,4 +26,17 @@ public class ServiceRibbonApplication {
         return new RestTemplate();
     }
 
+    /**
+     *  dashboard 404问题
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
