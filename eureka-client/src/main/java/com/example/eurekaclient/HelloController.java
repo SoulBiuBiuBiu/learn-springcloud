55c77498ffea2ea897c0d7a3dcbedd351a2fd922
Barry <1172807506@qq.com>
2021-06-17 15:22:30 +0800

    Feign 入门&参数绑定

--- eureka-client/src/main/java/com/example/eurekaclient/HelloController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/HelloController.java
@@ -5,8 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,15 +14,32 @@ public class HelloController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private DiscoveryClient client;
+
     @GetMapping(value = "/hello")
-    public String index(){
-        List<String> services=client.getServices();
+    public String index() {
+        List<String> services = client.getServices();
         logger.info(services.toString());
-        for (String service:
+        for (String service :
                 services) {
             List<ServiceInstance> instances = client.getInstances(service);
             logger.info(instances.toString());
         }
         return "Hello world!";
     }
+
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "hello" + name;
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "hello " + user.getName() + ", " + user.getAge();
+    }
+
 }
---
+++ feign-consumer/src/main/java/com/example/feignconsumer/HelloService.java
@@ -0,0 +1,20 @@
+package com.example.feignconsumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient("service-hi")
+public interface HelloService {
+    @RequestMapping("/hello")
+    String hello();
+
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    String hello(@RequestParam("name") String name);
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    String hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    String hello(@RequestBody User user);
+
+}
